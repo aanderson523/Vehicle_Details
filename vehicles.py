a806e94998e0977d769004e6d1e09acd22e473b7
Aubre A Anderson <aanderson523@ivytech.edu>
2022-09-06 22:35:33 -0400

    first commit

---
+++ vehicles.py
@@ -0,0 +1,27 @@
+
+
+class Vehicle:
+    def __init__(self, input, type):
+        self.input = input
+        self.type = type
+vehicleType = str(input("Enter Vehicle Type: "))
+
+
+
+class Automobile(Vehicle):
+    def __init__(self, input, type, year, make, model, doors, roof):
+        super().__init__(input, type)
+        self.year = year
+        self.make = make
+        self.model = model
+        self.doors = doors
+        self.roof = roof
+
+year = str(input("Enter Year: "))
+make = str(input("Enter Make: "))
+model = str(input("Enter Model: "))
+doors = str(input("Enter doors: "))
+roof = str(input("Enter roof: "))
+
+print("Type: " + vehicleType)
+print("Year: " +year,"Make: " + make,"Model: " + model, "Doors: " +  doors, "Roof: " + roof, sep="\n")
